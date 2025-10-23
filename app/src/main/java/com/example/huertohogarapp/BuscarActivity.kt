d25e9f7fa0fcc4f4c8337d3c72319daf46ca1232
brusvega <br.vegav@duocuc.cl>
2025-10-23 10:01:32 -0300

    123

--- app/src/main/java/com/example/huertohogarapp/BuscarActivity.kt
+++ app/src/main/java/com/example/huertohogarapp/BuscarActivity.kt
@@ -1,11 +1,56 @@
 package com.example.huertohogarapp
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONArray
 
 class BuscarActivity : AppCompatActivity() {
+
+    private lateinit var listView: ListView
+    private val productos = mutableListOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_buscar)
+
+        listView = findViewById(R.id.listViewResultados)
+
+        val url = "http://10.40.109.56/huertohogar_api/listar_productos.php"
+
+
+
+
+        cargarProductos(url)
+    }
+
+    private fun cargarProductos(url: String) {
+        val queue = Volley.newRequestQueue(this)
+
+        val request = JsonArrayRequest(
+            Request.Method.GET, url, null,
+            { response: JSONArray ->
+                productos.clear()
+                for (i in 0 until response.length()) {
+                    val obj = response.getJSONObject(i)
+                    val nombre = obj.getString("nombre")
+                    val precio = obj.getDouble("precio")
+                    productos.add("$nombre - $precio CLP")
+                }
+
+                val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, productos)
+                listView.adapter = adapter
+            },
+            { error ->
+                Toast.makeText(this, "Error: ${error.message}", Toast.LENGTH_LONG).show()
+            }
+        )
+
+        queue.add(request)
     }
 }
